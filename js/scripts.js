a10da6b5e3cd330400e9ef1bf2465b5649ba2775
Jeremy Kale Padot <jeremy@padot.us>
2020-01-31 11:53:08 -0800

    add working getPrice function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,34 @@
 // Business Logic
 
-
-
-
-
-
-
-
-
-//  User Logic
-
+function Pizza(size, crust, base, cheese, toppings, method, price) {
+  this.setSize = size;
+  this.setCrust = crust;
+  this.setBase = base;
+  this.setCheese = cheese;
+  this.toppings = toppings;
+  this.method = method;
+  this.price = price;
+  console.log(this);
+}
+Pizza.prototype.getPrice = function() {
+  console.log("setSize");
+  console.log(this.setSize);
+  if (this.setSize === "sm") {
+    this.price = 10;
+  } else if (this.setSize === "med") {
+    this.price = 12;
+  } else if (this.setSize === "lg") {
+    this.price = 14;
+  }
+  // return this.price;
+};
+Pizza.prototype.Crust = function() {
+  if (this.setCrust === "glutenFree") {
+    this.price += 1;
+  }
+};
+console.log(window.location);
+var myPizza = new Pizza("sm", "glutenFree", "pesto", "noCheese", "pepperoni", "delivery");
+console.log(myPizza);
+var Price = myPizza.getPrice();
+console.log("price",myPizza.price);
